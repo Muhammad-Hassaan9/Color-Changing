44f430315430e899727dfcc6dfa34967957d346b
Hassaan-Expert <hassaanshahzad087@gmail.com>
2026-09-11 18:37:55 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let container = document.querySelector(".container");
+let colorCode = document.querySelector(".color-code");
+
+function randomColorGenerator() {
+    let redValue = Math.floor(Math.random() * 256);
+    let greenValue = Math.floor(Math.random() * 256);
+    let blueValue = Math.floor(Math.random() * 256);
+    let color = `RGB(${redValue}, ${greenValue}, ${blueValue})`;
+    return color
+}
+
+function updateUI() {
+    let colorValue = randomColorGenerator();
+    container.style.backgroundColor = colorValue;
+    colorCode.textContent = colorValue;
+}
+
+setInterval(() => {
+    updateUI()
+}, 2000);
